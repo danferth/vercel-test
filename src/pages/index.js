d02d6561c06142aa01e95591c6205d930e9af9ef
danferth <danferth@gmail.com>
2020-05-19 16:38:49 -0700

    blog coming together

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,58 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 
 const App = ({ data }) => {
   return (
     <Layout>
-      <h1>{data.site.siteMetadata.title}</h1>
-      <div>
-        <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
+      <div
+        css={css`
+          border: 1px solid black;
+          margin-bottom: 1rem;
+        `}
+      >
+        <h1>{data.site.siteMetadata.title}</h1>
+        <div>
+          <img
+            src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+            alt="Group of pandas eating bamboo"
+          />
+        </div>
+      </div>
+      <div
+        css={css`
+          border: 1px solid teal;
+          padding: 1rem;
+        `}
+      >
+        <h1>Panda Blog</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts so far!</h4>
+
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                  font-size: 0.75rem;
+                `}
+              >
+                {"- "}
+                {node.frontmatter.date}
+                {" | "}
+                {node.frontmatter.author}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       </div>
     </Layout>
   )
@@ -21,6 +63,19 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            author
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+          excerpt
+        }
       }
     }
   }
