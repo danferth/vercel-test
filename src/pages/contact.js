700dc04c5c8fcedd77f8da522610ef199a0b988b
danferth <danferth@gmail.com>
2020-05-29 16:55:21 -0700

    removed headers att and it works

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,51 +6,17 @@ import Layout from "../components/layout"
 const Contact = () => {
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => {
-    let jsonData = JSON.stringify(data)
-    const fetchHeaders = new Headers({
-      Accept: "application/json",
-      "Content-type": "text/plain",
-    })
-    console.log("about to fetch something")
+    let formEntry = new FormData()
+    for (const entry in data) {
+      formEntry.append(`${entry}`, `${data[entry]}`)
+    }
     fetch("https://forms.danferth.com/parse/devferth-contact.php", {
       method: "POST",
-      mode: "no-cors",
-      credentials: "omit",
-      body: jsonData,
-      headers: { fetchHeaders },
+      mode: "cors",
+      body: formEntry,
     })
-      .then(responce => {
-        if (!responce.ok) {
-          throw new Error("there was a problem")
-        }
-        console.log("here we sit")
-        return responce.blob()
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    //   .then(responce => responce.json())
-    //   .then(data => console.log(data))
-    //   .catch(error => console.log(error))
-
-    // fetch("https://forms.danferth.com/parse/devferth-contact.php", {
-    //   method: "POST",
-    //   mode: "cors",
-    //   body: JSON.stringify(data),
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then(responce => responce.json())
-    //   .then(responce => {
-    //     if (responce.status === "success") {
-    //       console.log("Message Sent")
-    //       console.log(responce)
-    //     } else if (responce.status === "fail") {
-    //       console.log("message failes!")
-    //     }
-    //   })
+      .then(response => response.json())
+      .then(data => console.log(data))
   }
 
   return (
